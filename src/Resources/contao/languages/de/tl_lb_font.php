530ca12001b4980ef513911792abd23cdf70ac27
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2020-06-10 10:45:18 +0200

    Add files via upload

---
+++ src/Resources/contao/languages/de/tl_lb_font.php
@@ -0,0 +1,6 @@
+<?php 
+$GLOBALS['TL_LANG']['tl_lb_font']['primary_legend'] = "Fonts";
+
+$GLOBALS['TL_LANG']['tl_lb_font']['font1'] = array("Haupt-Schrifttype","Bitte auswählen, welches die hauptsächliche Firmen-/Markenschrift ist");
+$GLOBALS['TL_LANG']['tl_lb_font']['font2'] = array("Akzent-Schrifttype","Bitte auswählen, welches Schrifttype als Akzent-Schrifttype dienen soll");
+$GLOBALS['TL_LANG']['tl_lb_font']['resetToDefault'] = array("Zurücksetzen","Setzt alle Werte auf die Standardwerte zurück");
