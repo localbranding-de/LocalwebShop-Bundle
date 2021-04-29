ed3fc9454bedf88c8a840205a89b69a700ce2071
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-04-29 15:52:50 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -29,5 +29,70 @@ if ('BE' === TL_MODE) {
     
 }
 
+<?php
+
+/**
+ * notification_center extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2008-2015, terminal42
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    LGPL
+ */
+
+/**
+ * Back end modules
+ */
+array_insert($GLOBALS['BE_MOD'], 1, array
+    (
+        'calendar_booking' => array
+        (
+            'lblocation' => array
+            (
+                'tables'        => array('tl_lb_location')
+            ),
+            /**  'lb_exceptions' => array
+            (
+                'tables' => array('tl_lb_exception')    
+            ), */
+            'buisness_holiday_date'=> array
+            (
+                'tables' => array('tl_lb_holiday_business')    
+            )
+            /**,
+              'tl_lb_member_shop_products'=> array
+            (
+                'tables' => array('tl_lb_member_00_shop_products')
+            )*/
+        )
+    ));
+
+
+/**
+ * Front end modules
+ */
+
+// Registrieren im Hooks replaceInsertTags
+//$GLOBALS['TL_HOOKS']['replaceInsertTags'][] = array('\CalenderBooking\classes\modules\LbInserttagClass', 'myReplaceInsertTags');
+
+$GLOBALS['TL_LANG']['STATES']['BW']    = 'Baden Württemberg';
+$GLOBALS['TL_LANG']['STATES']['BY']    = 'Bayern';
+$GLOBALS['TL_LANG']['STATES']['BE']   = 'Berlin';
+$GLOBALS['TL_LANG']['STATES']['BB'] = 'Brandenburg';
+$GLOBALS['TL_LANG']['STATES']['HB']  = 'Bremen';
+$GLOBALS['TL_LANG']['STATES']['HH']    = 'Hamburg';
+$GLOBALS['TL_LANG']['STATES']['HE']  = 'Hessen';
+$GLOBALS['TL_LANG']['STATES']['MV']  = 'Mecklenburg-Vorpommern';
+$GLOBALS['TL_LANG']['STATES']['NI']  = 'Niedersachsen';
+$GLOBALS['TL_LANG']['STATES']['NW']  = 'Nordrhein-Westfalen';
+$GLOBALS['TL_LANG']['STATES']['RP']  = 'Rheinland-Pfalz';
+$GLOBALS['TL_LANG']['STATES']['SL']  = 'Saarland';
+$GLOBALS['TL_LANG']['STATES']['SN']  = 'Sachsen';
+$GLOBALS['TL_LANG']['STATES']['ST']  = 'Sachsen-Anhalt';
+$GLOBALS['TL_LANG']['STATES']['SH']  = 'Schleswig-Holstein';
+$GLOBALS['TL_LANG']['STATES']['TH']  = 'Thüringen';
+
+$GLOBALS['FE_MOD']['calendarbooking'] = array('lb_booking'=>'\CalendarBooking\classes\modules\easymodul');
+$GLOBALS['TL_FFL']['lb_calendarfield']     = LbCalendarFormField::class;
+$GLOBALS['FE_MOD']['miscellaneous']['locationselect'] = '\CalendarBooking\classes\modules\easymodul';
 
 
