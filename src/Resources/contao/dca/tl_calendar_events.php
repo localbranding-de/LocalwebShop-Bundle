b9c771c0aae57aa29db992c94c220ba12dabee12
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-04-30 10:38:15 +0200

    Update tl_calendar_events.php

--- src/Resources/contao/dca/tl_calendar_events.php
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -13,3 +13,19 @@ $GLOBALS['TL_DCA']['tl_calendar_events']['fields']['approved'] = array
     'default' => 1,
  	'sql'       => "char(1) NOT NULL default ''"
 );
+
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['bought'] = array
+(
+    'label' => &$GLOBALS['TL_LANG']['tl_calendar_events']['bought'],
+    'sql'       => "char(1) NOT NULL default '0'"
+);
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['updated'] = array
+(
+    'label' => &$GLOBALS['TL_LANG']['tl_calendar_events']['updated'],
+    'sql'                     => "int(10) unsigned NOT NULL default '0'"
+);
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields']['order'] = array
+(
+    'label' => &$GLOBALS['TL_LANG']['tl_calendar_events']['order'],
+    'sql'                     => "int(10) unsigned NOT NULL default '0'"
+);
