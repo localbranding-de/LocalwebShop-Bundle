c716fec30312d6e4c647451021a5dfba857e7c10
localbranding-de <53892311+localbranding-de@users.noreply.github.com>
2021-05-04 00:22:08 +0200

    Add files via upload

---
+++ src/Resources/contao/Classes/LbCalendarFormField.php
@@ -0,0 +1,36 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace CalendarBooking\classes;
+
+
+
+class LbCalendarFormField extends AbstractFormField
+{
+    /**
+     * Template name.
+     *
+     * @var string
+     */
+    protected $strTemplate = 'form_lb_calendar';
+
+    /**
+     * {@inheritDoc}
+     */
+}
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function generate()
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+}
